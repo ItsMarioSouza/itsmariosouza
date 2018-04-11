3fdeac064c97739af5de4193868a251cf9c0c972
Mario Souza <msouza@arn.com>
2018-04-11 14:19:35 -0400

    home grid

--- theme/index.php
+++ theme/index.php
@@ -45,35 +45,20 @@
 					<!-- Get Posts & Place Them Into Template -->
 					<?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-						<?php if( have_rows('home_grid_items_acf') ): while( have_rows('home_grid_items_acf') ): the_row(); ?>
-							<?php
-								// Feilds from parent repeater – grid items
-								$link = get_sub_field('link_acf');
-								$image = get_sub_field('image_acf');
-								$title = get_sub_field('title_acf');
-							?>
-
-							<?php if( have_rows('title_options_acf') ): while( have_rows('title_options_acf') ): the_row(); ?>
-								<?php
-									// Feilds from child repeater – title options
-									$color = get_sub_field('color_acf');
-									$visibility = get_sub_field('visibility_acf');
-								?>
-							<?php endwhile; endif; ?>
-
-							<li class="grid__item grid__item--home">
+						<?php if( have_rows('post_title_options_acf') ): while( have_rows('post_title_options_acf') ): the_row(); ?>
+							<article class="grid__item grid_item--home">
 								<a class="grid__item-link" href="<?php the_permalink(); ?>">
-									<img class="grid__item-img" src="<?php echo $image ?>" alt="" />
+									<img class="grid__item-img" src="<?php the_field('post_grid_image_acf'); ?>" alt="" />
 
 									<div class="grid__item-copy-container">
-										<h1 class="grid__item-title <?php echo $color ?> <?php echo $visibility ?>">
-											<span><!-- ADD CLIENT FIELD --></span>
+										<h1 class="grid__item-title <?php the_sub_field('color_acf'); ?> <?php the_sub_field('visibility_acf'); ?>">
+											<span></span>
 											<?php the_title(); ?>
 										</h1>
 									</div>
 								</a>
-							</li>
-						<?php endwhile; endif; //home grid items loop ?>
+							</article>
+						<?php endwhile; endif;  ?>
 
 
 					<?php endwhile; else : ?>
