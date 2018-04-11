7d9cec99841c490791af75ca36003366341d9e68
Mario Souza <msouza@arn.com>
2018-04-11 14:24:39 -0400

    Home grid

--- theme/index.php
+++ theme/index.php
@@ -27,50 +27,55 @@
 			</section>
 
 			<section class="grid">
-				<ul class="grid__list grid_list--home">
+				<?php if( !post_password_required($post) ): ?>
+					<ul class="grid__list grid_list--home">
 
-					<?php
-						// Arguments
-						$args = array(
-							'post_type' 		=> 'post',
-							'meta_key'			=> 'posts_order_acf',
-							'orderby'			=> 'meta_value',
-							'order'				=> 'ASC',
-						);
+						<?php
+							// Arguments
+							$args = array(
+								'post_type' 		=> 'post',
+								'meta_key'			=> 'posts_order_acf',
+								'orderby'			=> 'meta_value',
+								'order'				=> 'ASC',
+							);
 
-						// Query
-						$the_query = new WP_Query($args);
-					?>
+							// Query
+							$the_query = new WP_Query($args);
+						?>
 
-					<!-- Get Posts & Place Them Into Template -->
-					<?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+						<!-- Get Posts & Place Them Into Template -->
+						<?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-						<?php if( have_rows('post_title_options_acf') ): while( have_rows('post_title_options_acf') ): the_row(); ?>
-							<article class="grid__item grid__item--home">
-								<a class="grid__item-link" href="<?php the_permalink(); ?>">
-									<img class="grid__item-img" src="<?php the_field('post_grid_image_acf'); ?>" alt="" />
+							<?php if( have_rows('post_title_options_acf') ): while( have_rows('post_title_options_acf') ): the_row(); ?>
+								<article class="grid__item grid__item--home">
+									<a class="grid__item-link" href="<?php the_permalink(); ?>">
+										<img class="grid__item-img" src="<?php the_field('post_grid_image_acf'); ?>" alt="" />
 
-									<div class="grid__item-copy-container">
-										<h1 class="grid__item-title <?php the_sub_field('color_acf'); ?> <?php the_sub_field('visibility_acf'); ?>">
-											<span></span>
-											<?php the_title(); ?>
-										</h1>
-									</div>
-								</a>
-							</article>
-						<?php endwhile; endif;  ?>
+										<div class="grid__item-copy-container">
+											<h1 class="grid__item-title <?php the_sub_field('color_acf'); ?> <?php the_sub_field('visibility_acf'); ?>">
+												<span></span>
+												<?php the_title(); ?>
+											</h1>
+										</div>
+									</a>
+								</article>
+							<?php endwhile; endif;  ?>
 
 
-					<?php endwhile; else : ?>
-						<p><?php esc_html_e('Sorry, no posts to display.'); ?></p>
+						<?php endwhile; else : ?>
+							<p><?php esc_html_e('Sorry, no posts to display.'); ?></p>
 
-					<!-- Stop The Loop -->
-					<?php endif; ?>
+						<!-- Stop The Loop -->
+						<?php endif; ?>
 
-					<!-- Reset The Query -->
-					<?php wp_reset_query(); ?>
+						<!-- Reset The Query -->
+						<?php wp_reset_query(); ?>
 
-				</ul> <!-- /grid__list -->
+					</ul> <!-- /grid__list -->
+
+				<?php endif; //post_password_required ?>
+
+				<?php the_content(); ?>
 			</section>
 
 		</div> <!-- /contentContainer -->
