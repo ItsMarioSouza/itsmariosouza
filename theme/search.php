b1d5d3e6f39f8989cb7dc14dfb1fef2b1eea8b5e
Mario Souza <msouza@arn.com>
2018-04-11 16:48:27 -0400

    Search = 404 ACF

--- theme/search.php
+++ theme/search.php
@@ -7,10 +7,10 @@
 		<div class="contentContainer contentContainer--error" role="main">
 			<section class="error404">
 				<div class="error404__container">
-					<h1 class="error404__headline error__headline--option"><?php the_field('error404_headline_acf', 'options'); ?></h1>
+					<h1 class="error404__headline error__headline--option"><?php the_field('error_headline_acf', 'options'); ?></h1>
 
-					<div class="error4-4__content borderLink borderLink--option">
-						<?php the_field('error404_content_acf', 'options'); ?>
+					<div class="error404__content borderLink borderLink--option">
+						<?php the_field('error_content_acf', 'options'); ?>
 					</div>
 				</div>
 			</section>
