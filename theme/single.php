e239b16bdf0480c0dcc9ee0813fe453b36831f9f
Mario Souza <msouza@arn.com>
2018-04-16 15:29:09 -0400

    Post template updates

--- theme/single.php
+++ theme/single.php
@@ -11,15 +11,12 @@
 
 					<section class="post__hero">
 						<div class="post__hero-container">
-							<img src="<?php the_field('post_hero-image_acf'); ?>" />
+							<img src="/etc/test.png" />
 						</div>
 					</section>
 
-					<section class="post__content">
 
-<?php if( have_rows('post_hero-image_acf') ): while( have_rows('post_hero-image_acf') ): the_row(); ?>
-<?php endwhile; endif; ?>
-
+					<?php if( !post_password_required($post) ): ?>
 						<?php
 							if( have_rows('post_content_acf') ): while ( have_rows('post_content_acf') ) : the_row();
 
@@ -37,12 +34,10 @@
 									elseif($columns == '2'):
 										echo '<div class="post__row"><div class="post__image post__image--double"><div class="post__image-container"><img src="' . $imageOne . '" /></div><div class="post__image-container"><img src="' . $imageTwo . '" /></div></div></div>';
 									endif;
-
 								endif;
 
 							endwhile; endif;
 						?>
-					</section>
 
 					<?php
 						// End if post_password_required
@@ -50,18 +45,19 @@
 						// include content if needed for password form
 						the_content();
 					?>
+
 				<?php endwhile; endif; ?>
 			</article>
 
-			<section>
-				<?php
-					// ACF Fields
-					if( have_rows('post_back_link_acf', 'option') ): while( have_rows('post_back_link_acf', 'option') ): the_row();
-						$icon = get_sub_field('icon_acf');
-						$text = get_sub_field('text_acf');
-					endwhile; endif;
-				?>
+			<?php
+				// ACF Fields
+				if( have_rows('post_back_link_acf', 'option') ): while( have_rows('post_back_link_acf', 'option') ): the_row();
+					$icon = get_sub_field('icon_acf');
+					$text = get_sub_field('text_acf');
+				endwhile; endif;
+			?>
 
+			<section>
 				<div class="arrowLink arrowLink--back">
 					<a href="/work">
 						<i class="<?php echo $icon ?>"></i>
