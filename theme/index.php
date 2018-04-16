277f8b4d3bcfba1c1bb5fba406612c78cfca1f48
Mario Souza <msouza@arn.com>
2018-04-16 19:05:13 -0400

    test

--- theme/index.php
+++ theme/index.php
@@ -13,50 +13,57 @@
 				</div>
 			</section>
 
-			<?php if( !post_password_required($post) ): ?>
 				<section class="grid">
 					<h2 class="grid__title"><?php the_field('home_grid_title_acf'); ?></h2>
 
-					<div class="grid__list grid_list--home">
-						<?php if( have_rows('home_grid_items_acf') ): while( have_rows('home_grid_items_acf') ): the_row(); ?>
-							<?php
-								// Feilds from parent repeater – grid items
-								$post_object = get_sub_field('post_acf');
-								if( $post_object ) : $post = $post_object;
-							?>
-
-							<?php if( have_rows('title_options_acf') ): while( have_rows('title_options_acf') ): the_row(); ?>
+					<?php if( ! post_password_required() ): ?>
+						<div class="grid__list grid_list--home">
+							<?php if( have_rows('home_grid_items_acf') ): while( have_rows('home_grid_items_acf') ): the_row(); ?>
 								<?php
-									// Feilds from child repeater – title options
-									$color = get_sub_field('color_acf');
-									$visibility = get_sub_field('visibility_acf');
+									// Feilds from parent repeater – grid items
+									$post_object = get_sub_field('post_acf');
+									if( $post_object ) : $post = $post_object;
 								?>
+
+								<?php if( have_rows('title_options_acf') ): while( have_rows('title_options_acf') ): the_row(); ?>
+									<?php
+										// Feilds from child repeater – title options
+										$color = get_sub_field('color_acf');
+										$visibility = get_sub_field('visibility_acf');
+									?>
+								<?php endwhile; endif; ?>
+
+								<?php if( have_rows('post_details_list_acf') ): while( have_rows('post_details_list_acf') ): the_row(); ?>
+									<?php
+										// Feilds from child repeater – title options
+										$client = get_sub_field('client_acf');
+									?>
+								<?php endwhile; endif; ?>
+
+								<article class="grid__item grid__item--home">
+									<a class="grid__item-link" href="<?php the_permalink(); ?>">
+										<img class="grid__item-img" src="<?php the_field('post_grid_image_acf'); ?>" alt="" />
+
+										<div class="grid__item-copy-container">
+											<h1 class="grid__item-title <?php echo $color ?> <?php echo $visibility ?>">
+												<span><?php echo $client ?></span>
+												<?php the_title(); ?>
+											</h1>
+										</div>
+									</a>
+								</article>
+
+								<?php wp_reset_postdata(); endif; //ACF Post Object ?>
+
 							<?php endwhile; endif; ?>
+						</div> <!-- /grid__list -->
 
-							<?php if( have_rows('post_details_list_acf') ): while( have_rows('post_details_list_acf') ): the_row(); ?>
-								<?php
-									// Feilds from child repeater – title options
-									$client = get_sub_field('client_acf');
-								?>
-							<?php endwhile; endif; ?>
+					<?php else: // If password is needed ?>
+						<section>
+							<?php the_content(); ?>
+						</section>
 
-							<article class="grid__item grid__item--home">
-								<a class="grid__item-link" href="<?php the_permalink(); ?>">
-									<img class="grid__item-img" src="<?php the_field('post_grid_image_acf'); ?>" alt="" />
-
-									<div class="grid__item-copy-container">
-										<h1 class="grid__item-title <?php echo $color ?> <?php echo $visibility ?>">
-											<span><?php echo $client ?></span>
-											<?php the_title(); ?>
-										</h1>
-									</div>
-								</a>
-							</article>
-
-							<?php wp_reset_postdata(); endif; //ACF Post Object ?>
-
-						<?php endwhile; endif; ?>
-					</div> <!-- /grid__list -->
+					<?php endif; // password protect ?>
 
 					<?php if( have_rows('home_grid_cta_acf') ): while( have_rows('home_grid_cta_acf') ): the_row(); ?>
 						<div class="grid__cta grid__cta--home">
@@ -67,12 +74,6 @@
 					<?php endwhile; endif; ?>
 				</section> <!-- /grid -->
 
-			<?php else: // If password is needed ?>
-				<section>
-					<?php the_content(); ?>
-				</section>
-
-			<?php endif; // password protect ?>
 		</div> <!-- /contentContainer -->
 
 <?php get_footer(); ?>
