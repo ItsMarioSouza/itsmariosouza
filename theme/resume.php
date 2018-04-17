a6f6d6fbc12a3a37b524c85777ec92a932516817
Mario Souza <msouza@arn.com>
2018-04-17 16:21:12 -0400

    Resume updates

--- theme/resume.php
+++ theme/resume.php
@@ -9,10 +9,10 @@
 		<div class="contentContainer contentContainer--resume" role="main">
 			<?php if( ! post_password_required() ): //If password is not needed ?>
 				<section class="res__intro">
-					<h1 class="res__intro-title">Mario Souza</h1>
+					<h1 class="res__intro-title">Résumé</h1>
 
 					<div class="res__intro-utilities">
-						<a class="icon__container res__icon-container about__icon-container--link" href="#" target="_blank">
+						<a class="icon__container res__icon-container about__icon-container--link" href="/wp-content/uploads/2018/04/mario-souza-resume-web-oct-2017.pdf" target="_blank">
 							<i class="fal fa-file-alt"></i>
 							<span class="icon__text res__icon-text">Download Résumé</span>
 						</a>
