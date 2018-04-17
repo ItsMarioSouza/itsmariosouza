97f10333471d421f5b6d5de9a0d0b6e5cc6ec9c2
Mario Souza <msouza@arn.com>
2018-04-17 00:27:34 -0400

    Resume template + footer

--- theme/resume.php
+++ theme/resume.php
@@ -8,16 +8,27 @@
 		<?php if( ! post_password_required() ): //If password is not needed ?>
 
 			<div class="contentContainer contentContainer--resume" role="main">
+				<section class="post__intro">
+					<h1 class="post__intro-title">Mario Souza</h1>
+
+					<div class="post__intro-icons">
+						<a class="about__icon-container about__icon-container--link" href="#" target="_blank">
+							<i class="fal fa-file-alt"></i>
+							<span class="about__icon-text">Résumé</span>
+						</a>
+					</div>
+				</section>
+
 				<section class="">
 
 				</section>
+
+				<aside class="">
+					<section class="">
+
+					</section>
+				</aside> <!-- /aside -->
 			</div> <!-- /main -->
-
-			<aside class="">
-				<section class="">
-
-				</section>
-			</aside> <!-- /aside -->
 
 		<?php
 			//If password is needed
