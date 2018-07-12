a2dfb08d8f641f3035952ffce8d8e88b21705af5
Mario Souza <mario@myriadweb.com>
2018-07-12 11:00:25 -0400

    Dandelion test

--- theme/resume.php
+++ theme/resume.php
@@ -7,7 +7,7 @@
 ?>
 
 		<div class="contentContainer contentContainer--resume" role="main">
-			<h1 class="res__intro-title">Test</h1>
+			<h1 class="res__intro-title">Test1</h1>
 
 		</div> <!-- /main -->
 
