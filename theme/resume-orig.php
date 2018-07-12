f1e585adb5addeb35dc3e3b5f9115d95e9298392
Mario Souza <mario@myriadweb.com>
2018-07-12 10:51:11 -0400

    Dandelion test

---
+++ theme/resume-orig.php
@@ -0,0 +1,292 @@
+<?php
+	/*
+	Template Name: Resume
+	*/
+
+	get_header();
+?>
+
+		<div class="contentContainer contentContainer--resume" role="main">
+			<h1 class="res__intro-title"><?php the_title(); ?></h1>
+
+			<?php if( ! post_password_required() ): //If password is not needed ?>
+				<section class="res__intro">
+					<div class="res__intro-utilities">
+						<a class="icon__container res__icon-container about__icon-container--link" href="/wp-content/uploads/2018/06/mario-souza-resume-web.pdf" target="_blank">
+							<i class="fal fa-file-alt"></i>
+							<span class="icon__text res__icon-text">Download PDF</span>
+						</a>
+					</div>
+				</section>
+
+				<div class="res__flex-container">
+					<div class="res__main-container">
+						<section>
+							<h2 class="res__section-title">Professional Experience</h2>
+
+							<div class="res__block">
+								<h3 class="res__block-title">Digital Producer</h3>
+
+								<div class="res__blockIntro">
+									<ul class="res__blockIntro-list">
+										<li>Arnold Worldwide</li>
+										<li>Boston, MA</li>
+										<li>August 2014 – Present</li>
+									</ul>
+
+									<div class="res__blockIntro-aux">
+										<p>Clients: CFP Board, Jack Daniel’s, Ocean Spray, Santander Bank, SolarCity and Volvo Trucks</p>
+									</div>
+								</div>
+
+								<div class="res__blockDetails">
+									<div class="res__blockDetails-copy">
+										<p>Produce high-quality products and experiences across technology. Aided the agency with the switch from Flash to HTLM5 display ads. Client facing and allocated through entire project life cycles. Bridge multiple departments to facilitate integration, foster innovation and design on brand. Responsible for work statements, production schedules, project budgets and managing both internal and external resources.</p>
+									</div>
+								</div>
+
+								<div class="res__blockDetails res__blockDetails--client">
+									<h4 class="res__blockDetails-title">Ocean Spray – Dot Com</h4>
+
+									<ul class="res__blockDetails-list">
+										<li>Lead digital producer of a global flagship website</li>
+										<li>Managed the internal front-end and Client back-end development, analytics, & QA of the site, which has been adapted for 40+ countries and translated into 10+ languages</li>
+										<li>Coordinated a WCAG 2.0 AA site update for the APAC market</li>
+										<li>Worked with Client’s global and in-market teams to determine content for each new site, then prepared copy for translation and gathered assets for retouching</li>
+										<li>Oversaw quarterly analytics reporting and improvement recommendations for the US site, email campaigns, & media</li>
+									</ul>
+								</div>
+
+								<div class="res__blockDetails res__blockDetails--client">
+									<h4 class="res__blockDetails-title">Ocean Spray – Mocktails</h4>
+
+									<ul class="res__blockDetails-list">
+										<li>Lead digital producer of an integrated campaign with 2 separate targets to launch a new product line</li>
+										<li>Managed the UX/UI design, development, QA, & analytics of 2 landing pages, each appealing to their target. Pages housed product information, a store locator, & shareable content like recipes and videos</li>
+										<li>Handled the design, development, QA testing, & tracking of a banner ad concept per target</li>
+										<li>Re-formatted final video content for social and site to meet specs using FFmpeg</li>
+										<li>Consulted, organized, & prototyped social content, both organic and paid units like Carousel and Canvas</li>
+									</ul>
+								</div>
+
+								<div class="res__blockDetails res__blockDetails--client">
+									<h4 class="res__blockDetails-title">CenturyLink – Sing with Kelvin</h4>
+
+									<ul class="res__blockDetails-list">
+										<li>Lead digital producer of an integrated campaign to highlight the power of the internet</li>
+										<li>Managed the UX/UI design, development, QA, & analytics of a microsite with 5 unique iterations, which housed an array of content from contest information to participant submissions</li>
+										<li>Prepared the client facing schedule based on media and content production dates</li>
+										<li>Determined overall campaign KPIs and user flow with a cross-disciplinary agency team</li>
+										<li>Coordinated with an independent vendor to write terms and determine the winner</li>
+									</ul>
+								</div>
+
+								<div class="res__blockDetails res__blockDetails--client">
+									<h4 class="res__blockDetails-title">Jack Daniel’s – Barrel Hunt</h4>
+
+									<ul class="res__blockDetails-list">
+										<li>Digital producer of an integrated campaign to celebrate the 150th anniversary of the Jack Daniel Distillery</li>
+										<li>Managed the UX/UI design, development, QA, and analytics of a landing page, which was the central hub of a global scavenger hunt for 150 barrels, which informed users of upcoming/found barrels, rules, and winner details</li>
+										<li>Partnered with Client’s global and in-market teams to gather content from each scavenger hunt winner in real time, which was adapted and uploaded to the site</li>
+										<li>Coordinated with Client’s IT team to host, push updates, & scape their Facebook page to pull in content</li>
+										<li>Handled the design, development, and QA of an email template</li>
+									</ul>
+								</div>
+							</div> <!-- /res__block -->
+
+							<div class="res__block">
+								<h3 class="res__block-title">Associate Digital Producer</h3>
+
+								<div class="res__blockIntro">
+									<ul class="res__blockIntro-list">
+										<li>Myriad Inc.</li>
+										<li>Boston, MA</li>
+										<li>October 2012 - January 2013</li>
+									</ul>
+
+									<div class="res__blockIntro-aux">
+										<p>Clients: Massachusetts General Hospital Cancer Center, MIT AgeLab, The National Center on Time & Learning, North Shore-LIJ Health System and Stonybrook Water</p>
+									</div>
+								</div>
+
+								<div class="res__blockDetails">
+									<div class="res__blockDetails-copy">
+										<p>Oversaw all digital projects from inception and carried them through final QA prior to launch. Administered schedules, daily hot sheets and resource allocations. Executed organic SEO campaigns through content generation, HTML modifications and link building. Delivered keyword and analytics reports. Developed sitemaps and designed wireframes. Consulted on social media and blogging.</p>
+									</div>
+								</div>
+							</div> <!-- /res__block -->
+
+							<div class="res__block">
+								<h3 class="res__block-title">Project Management Intern</h3>
+
+								<div class="res__blockIntro">
+									<ul class="res__blockIntro-list">
+										<li>Myriad Inc.</li>
+										<li>Boston, MA</li>
+										<li>October 2012 - January 2013</li>
+									</ul>
+								</div>
+
+								<div class="res__blockDetails">
+									<div class="res__blockDetails-copy">
+										<p>Learned the operations of a graphic design, web development and marketing agency. Exposed to multiple projects at any single point in time. Became a more valuable team member by teaching myself new skills such as CSS and basic audio editing.</p>
+									</div>
+								</div>
+							</div> <!-- /res__block -->
+						</section>
+
+						<section>
+							<h2 class="res__section-title">Education</h2>
+
+							<div class="res__block">
+								<h3 class="res__block-title">Bachelor of Science in Marketing & Minor in International Business</h3>
+
+								<div class="res__blockIntro">
+									<ul class="res__blockIntro-list">
+										<li>University of Massachusetts Dartmouth</li>
+										<li>North Dartmouth, MA</li>
+										<li>Class of 2011</li>
+									</ul>
+								</div>
+
+								<div class="res__blockDetails">
+									<ul class="res__blockDetails-list">
+										<li><span>Awards: </span>Dean’s List - Major GPA of 3.8 - Cum Laude Distinction</li>
+										<li><span>Study Abroad Experience: </span>spent the summer of 2010 in Wolfsburg, Germany to learn about Volkswagen’s global marketing efforts, strategies and tactics.</li>
+										<li><span>Internship: </span>completed an ongoing sales internship at the Fastenal Company junior and senior years.</li>
+									</ul>
+								</div>
+							</div> <!-- /res__block -->
+						</section>
+
+						<section>
+							<h2 class="res__section-title">Academic Experience</h2>
+
+							<div class="res__block">
+								<h3 class="res__block-title">Market Researcher</h3>
+
+								<div class="res__blockIntro">
+									<ul class="res__blockIntro-list">
+										<li>Fall River Historical Society</li>
+										<li>Fall River, MA</li>
+										<li>Spring Semester 2011</li>
+									</ul>
+								</div>
+
+								<div class="res__blockDetails">
+									<div class="res__blockDetails-copy">
+										<p>Conducted market research for the Society in order to understand attitudes of tour companies towards offering historical tours. Identified a number of tour companies interested in offering tours planned by the Society, which proved to generate much needed business.</p>
+									</div>
+								</div>
+							</div> <!-- /res__block -->
+
+							<div class="res__block">
+								<h3 class="res__block-title">Social Media Consultant</h3>
+
+								<div class="res__blockIntro">
+									<ul class="res__blockIntro-list">
+										<li>Cranberry Marketing Committee</li>
+										<li>Wareham, MA</li>
+										<li>Fall Semester 2010</li>
+									</ul>
+								</div>
+
+								<div class="res__blockDetails">
+									<div class="res__blockDetails-copy">
+										<p>Met with the Committee and followed their existing social media efforts, along with efforts of other similar commodity food groups over the course of a semester. Coordinated my group to create a strategic social media campaign that allowed the Committee to better their efforts and drive increased awareness.</p>
+									</div>
+								</div>
+							</div> <!-- /res__block -->
+						</section>
+					</div> <!-- /res__main-container -->
+
+					<aside class="res__aside-container">
+						<section class="">
+							<div class="res__block">
+								<h2 class="res__section-title">Contact</h2>
+
+								<div class="res__blockDetails">
+									<ul class="res__blockDetails-list">
+										<li>Boston, MA</li>
+										<li><a href="mailto:mario@itsmariosouza.com">mario@itsmariosouza.com</a></li>
+									</ul>
+								</div>
+							</div> <!-- /res__block -->
+						</section>
+
+						<section class="">
+							<div class="res__block">
+								<h2 class="res__section-title">Social</h2>
+
+								<div class="res__blockDetails">
+									<ul class="res__blockDetails-list">
+										<li>
+											<a class="icon__container res__icon-container about__icon-container--link" href="https://www.linkedin.com/in/itsmariosouza/" target="_blank">
+												<i class="fab fa-linkedin-in"></i>
+												<span class="icon__text res__icon-text">LinkedIn</span>
+											</a>
+										</li>
+										<li>
+											<a class="icon__container res__icon-container about__icon-container--link" href="https://www.instagram.com/itsmariosouza/" target="_blank">
+												<i class="fab fa-instagram"></i>
+												<span class="icon__text res__icon-text">Instagram</span>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div> <!-- /res__block -->
+						</section>
+
+						<section class="">
+							<div class="res__block">
+								<h2 class="res__section-title">Certificates</h2>
+
+								<div class="res__blockDetails">
+									<ul class="res__blockDetails-list res__blockDetails-list--extra-spacing">
+										<li>
+											<span>JavaScript Circuit – </span>distributed by General Assembly
+										</li>
+										<li>
+											<span>Certified ScrumMaster&reg; – </span>distributed by Scrum Alliance
+										</li>
+										<li>
+											<span>Inbound Marketing Certified –  </span>distributed by HubSpot
+										</li>
+									</ul>
+								</div>
+							</div> <!-- /res__block -->
+						</section>
+
+						<section class="">
+							<div class="res__block">
+								<h2 class="res__section-title">Skills</h2>
+
+								<div class="res__blockDetails">
+									<div class="res__blockDetails-copy">
+										<p><span>Everyday Tools Used: </span>Mac OS X, Slack, Google Drive, Smartsheet, Jira, Trello, Keynote, Photoshop, Atom and FTP</p>
+										<p><span>Other Proficiencies: </span>Excel, PowerPoint, Word, HTML, CSS, JavaScript, jQuery, Gulp, WordPress, Drupal, Git, FFmpeg, InDesign, Illustrator, Acrobat, Google Analytics, social media platforms, Microsoft Project, Audacity, MOZ and Articulate Storyline</p>
+									</div>
+								</div>
+							</div> <!-- /res__block -->
+						</section>
+					</aside> <!-- /res__aside-container -->
+				</div> <!-- /res__flex-container -->
+
+			<?php else: //If password is needed ?>
+				<section>
+					<?php the_content(); ?>
+				</section>
+
+				<section>
+					<div class="arrowLink arrowLink--back">
+						<a href="/">
+							<i class="fal fa-angle-left"></i>
+							<span>Back Home</span>
+						</a>
+					</div>
+				</section>
+
+			<?php endif; //End password protect ?>
+		</div> <!-- /main -->
+
+<?php get_footer(); ?>
