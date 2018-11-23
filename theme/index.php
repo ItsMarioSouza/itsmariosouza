a5125114d972bd73b42c4700a30f65b844bfa920
Mario Souza <mario@Marios-MacBook-Pro.local>
2018-11-23 00:31:17 -0500

    Responsive images

--- theme/index.php
+++ theme/index.php
@@ -35,14 +35,21 @@
 
 								<?php if( have_rows('post_details_list_acf') ): while( have_rows('post_details_list_acf') ): the_row(); ?>
 									<?php
-										// Feilds from child repeater – title options
+										// Feilds from child repeater
 										$client = get_sub_field('client_acf');
 									?>
 								<?php endwhile; endif; ?>
 
 								<article class="grid__item grid__item--home">
 									<a class="grid__item-link" href="<?php the_permalink(); ?>">
-										<img class="grid__item-img" src="<?php the_field('post_grid_image_acf'); ?>" alt="" />
+
+										<?php
+											$image = get_field('post_grid_image_acf');
+											$size = 'full';
+											if($image) {
+												echo wp_get_attachment_image($image, $size, false, ['class' => 'grid__item-img']);
+											}
+										?>
 
 										<div class="grid__item-copy-container">
 											<h1 class="grid__item-title <?php echo $color ?> <?php echo $visibility ?>">
