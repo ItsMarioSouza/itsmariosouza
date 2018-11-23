497b908ddaae36f0cc05de08b1d49aa76d9c3536
Mario Souza <mario@Marios-MacBook-Pro.local>
2018-11-23 00:07:53 -0500

    Test

--- theme/page.php
+++ theme/page.php
@@ -60,9 +60,16 @@
 								<article class="grid__item grid__item--blog" data-aos="<?php echo $animation ?>">
 									<a class="grid__item-link" href="<?php the_permalink(); ?>">
 
-										<img class="grid__item-img" src="<?php the_field('post_grid_image_acf'); ?>" alt="" />
+										<!-- <img class="grid__item-img" src="<?php the_field('post_grid_image_acf'); ?>" alt="" /> -->
 
-										
+										<?php
+											$image = get_field('post_grid_image_acf');
+											$size = 'full';
+											if($image) {
+												echo wp_get_attachment_image($image, $size);
+											}
+											endif;
+										?>
 
 										<div class="grid__item-copy-container">
 											<h1 class="grid__item-title <?php the_sub_field('color_acf'); ?> <?php the_sub_field('visibility_acf'); ?>">
